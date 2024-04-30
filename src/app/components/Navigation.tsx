d625e04e5223c6293871b41219129f1ca19aae8a
AvotraIsmael <avotraismael4@gmail.com>
2024-04-30 14:05:23 +0100

    IAN: correction link

--- src/app/components/Navigation.tsx
+++ src/app/components/Navigation.tsx
@@ -1,19 +1,10 @@
-'use client'
 import Link from 'next/link'
-import { usePathname } from 'next/navigation'
 import React from 'react'
 
 const Navigation = ({ textBtn }: { textBtn: string }) => {
-  const pathname = usePathname()
-
-  const getLanguageWithoutSlash = () => {
-    const pathParts = pathname.split('/')
-    return pathParts.length > 1 ? pathParts[1] : 'en'
-  }
-
   return (
     <Link
-      href={`/${getLanguageWithoutSlash()}/profile`}
+      href={`/profile`}
       className='flex items-center gap-2 bg-pink rounded justify-center py-2 tablet:py-3 desktop:w-[50%] text-black font-medium'
     >
       {textBtn}
